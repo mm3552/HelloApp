87f3f5c8a901bcde1bcd35a2d40ea07b9d2fcc8d
M Aditya <mm3552@srmist.edu.in>
2026-04-06 10:39:02 +0530

    Update HelloApp.java

--- HelloApp.java
+++ HelloApp.java
@@ -1,64 +1,46 @@
 /**
-* HelloApp UC6 Display "Hello" with Multiple Command-Line Arguments using
-* substring to Remove Trailing Delimiter
+* HelloApp UC7 A simple Java application that greets multiple users by name
+* if provided as command-line arguments using the String.join() method, or defaults.
+* to greeting "World" if no names
 *
-* UC 6: Display "Hello" with Multiple Command-Line Arguments using substring to
-* Remove Trailing Delimiter The application should accept multiple names as * command-line arguments and display a personalized greeting for each user using
-* substring to remove the trailing delimiter.
-* Usage: java HelloApp [name1] [name2] [nameN] ...
-* If names are provided, it will display "Hello, [Name1], [Name2], ...!" to the console. *
-* If no names are provided, it will display "Hello, World!"
+* UC 7: Display "Hello" with Multiple Command-Line Arguments using String.join() Methoc
+* or Default Message The application should accept multiple names as command-line
+* arguments and display a personalized greeting for each user using the String.join()
+* method. If no names are provided, it should display "Hello, World!".
+* Usage: java HelloApp [name1] [name2] ... [nameN]
 *
-* @author Developer M Aditya
-* @version 6.0
+If names are provided, it will display "Hello, [Name1], [Name2], ...!" to the
+* console.
+If no names are provided, it will display "Hello, World!"
+* @author M Aditya
+* @version 7.0
 * @since UC1
 */
 
-// Key Concepts for HelloApp UC6:
-// 1. Enhanced For Loop: A simplified syntax for iterating over arrays or collections
-//without needing an index variable.
-// 2. StringBuilder: A mutable sequence of characters used for efficient string
-//concatenation.
-// 3. String Manipulation: Using methods like 'substring() to modify strings after
-//construction.
-// 4. Trailing Character Removal: Techniques to remove unwanted characters (like a //comma and space) from the end of a string after building it.
-// 5. String Length: Understanding how to use the length() method to determine //the size of a string and manipulate it accordingly.
-
-// Sample Code for HelloApp UC6:
-// StringBuilder nameBuilder = new StringBuilder();
-// for (String name args) {
-//    nameBuilder.append(name).append(",");
-// }
-//
-// if (nameBuilder.length() > 0) {
-//        name = nameBuilder.substring(0, nameBuilder.length()-2); // Remove the last ",
+// Key Concepts for HelloApp UC 4C:
+// 1. String.join() Method: A built-in method to concatenate strings with a delimiter
+// 2. Command-line Arguments: Accessing user input via args[] parameter
+// 3. Conditional Statements: Using if to check conditions
+// 4. Boolean Logic: Using logical conditions to control flow
+// 5. Array Length: Checking the number of command-line arguments
+// 6. Code Simplification: Reducing code complexity by leveraging built-in methods
+// 7. Readability and Maintainability: Writing cleaner code with fewer lines and less.
+//manual string manipulation
+// Sample Code for HelloApp UC 4C:
+// String names = "World";
+// if (args.length > 0) {
+//names = String.join(", ", args);
 // }
 
-public class HelloApp 
-{
-    public static void main(String[] args) 
-    {
+public class HelloApp {
+    public static void main(String[] args) {
         String greeting;
 
         if (args.length == 0) {
             greeting = "Hello, World!";
-        } 
-        else 
-        {
-            StringBuilder nameBuilder = new StringBuilder();
-
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
-
-            if (nameBuilder.length() > 0) {
-                String names = nameBuilder.substring(0, nameBuilder.length() - 2);
-                greeting = "Hello, " + names + "!";
-            } 
-            else 
-            {
-                greeting = "Hello, World!";
-            }
+        } else {
+            String names = String.join(", ", args);
+            greeting = "Hello, " + names + "!";
         }
 
         System.out.println(greeting);
